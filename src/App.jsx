0235812ca69e120e4e34c250b6cd9c3c009b4bbc
Lehvasconceloss <levasconcelos2002@hotmail.com>
2025-06-12 21:55:27 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
     <div>
       <h1>Capítulo Vivo</h1>
-      <p>Seu assistente de escrita interativo com Vite + React + Firebase.</p>
+      <p>Projeto React + Vite corrigido.</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
